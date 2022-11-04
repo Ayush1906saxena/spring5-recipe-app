f23c5223ab2a1dd2efd94ac07af1a861edff5bae
Ayush Saxena <ayushsaxenarockz1906@gmail.com>
2022-11-04 21:01:05 +0530

    Added functionality to update recipe.

--- src/test/java/guru/springframework/spring5recipeapp/converters/IngredientToIngredientCommandTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/converters/IngredientToIngredientCommandTest.java
@@ -49,7 +49,8 @@ class IngredientToIngredientCommandTest {
         //when
         IngredientCommand ingredientCommand = converter.convert(ingredient);
         //then
-        assertNull(ingredientCommand.getUnitOfMeasure());
+        assert ingredientCommand != null;
+        assertNull(ingredientCommand.getUom());
         assertEquals(ID_VALUE, ingredientCommand.getId());
         // assertEquals(RECIPE, ingredientCommand.get);
         assertEquals(AMOUNT, ingredientCommand.getAmount());
@@ -72,9 +73,10 @@ class IngredientToIngredientCommandTest {
         //when
         IngredientCommand ingredientCommand = converter.convert(ingredient);
         //then
+        assert ingredientCommand != null;
         assertEquals(ID_VALUE, ingredientCommand.getId());
-        assertNotNull(ingredientCommand.getUnitOfMeasure());
-        assertEquals(UOM_ID, ingredientCommand.getUnitOfMeasure().getId());
+        assertNotNull(ingredientCommand.getUom());
+        assertEquals(UOM_ID, ingredientCommand.getUom().getId());
         // assertEquals(RECIPE, ingredientCommand.get);
         assertEquals(AMOUNT, ingredientCommand.getAmount());
         assertEquals(DESCRIPTION, ingredientCommand.getDescription());
